bb141b1dbca76073a139c1bd4eae5a66d5f6dcee
yanx <yanx@oupeng.com>
2013-07-01 16:57:28 +0800

    补充注释 为mask控件增加动画效果

--- demo/widget/mask/mask.js
+++ demo/widget/mask/mask.js
@@ -1,18 +1,28 @@
 ;(function(o, undefined) {
 
     "use strict";
+
+    /**
+     * @param animation {String} 具体支持动画类型 请见<octopus.Widget.Mask>
+     * @type {o.Widget.Mask}
+     */
     var mask = new o.Widget.Mask({
-        isScroll: true
+        isScroll: false,
+        animation: "slideUp"
     });
+
     mask.on("tap", function() {
-        console.log(11111);
         mask.hidden();
     });
+
     var _mask = mask.clone();
+
     _mask.on("tap", function() {
         _mask.hidden();
     })
+
     o.event.on(window, "DOMContentLoaded", onLoadCompleted, false);
+
     function onLoadCompleted() {
         o.gesture(o.g("global_mask")).on("tap", function() {
             mask.active ? mask.show() : mask.render();
--- src/widget/mask/mask.js
+++ src/widget/mask/mask.js
@@ -8,9 +8,11 @@
  * @require lib/util.js
  * @require lib/dom.js
  * @require lib/event.js
+ * @require lib/tween.js
+ * @require lib/animate.js
  * @require widget/widget.js
  */
-;console.log("mask");;(function(o, undefined) {
+;(function(o, undefined) {
 
     "use strict";
 
@@ -18,7 +20,13 @@
      * @class octopus.Widget.Mask
      * @parent octopus.Widget
      * @desc 浮层遮罩
-     * @type {*|new|new}
+     * @param options {Object} 参数
+     * @param options.isScroll {Boolean} 浮层浮出时，是否禁掉背后的滚动条事件，false禁掉，默认值为false
+     * @param options.animation {String} 浮层浮出的动化类型，默认无动画 支持的类型有
+     * "fade" -- 渐隐渐出
+     * "scale" -- 中部呼出
+     * "rotate" -- 左上角转入 左下角转出
+     * "slideLeft", "slideRight", "slideUp", "slideDown" -- 与<octopus.animation>保持一致
      */
     o.Widget.Mask = o.define(o.Widget, {
 
@@ -34,6 +42,7 @@
          * @private
          * @property isResize
          * @type {Boolean}
+         * @desc 标志位 判断当前是否处在resize事件执行
          */
         isResize: false,
 
@@ -41,14 +50,9 @@
          * @private
          * @property animation
          * @type {Boolean}
+         * @desc 参数 表明浮层浮出的动画类型
          */
-        animation: true,
-
-        /**
-         * @private
-         * @property toOpacity
-         */
-        toOpacity: .5,
+        animation: null,
 
         /**
          * @private
@@ -62,6 +66,7 @@
         /**
          * @private
          * @method initEvent
+         * @desc 初始化事件
          */
         initEvent: function() {
             !this.isScroll && o.event.on(document, "touchmove", o.util.bindAsEventListener(this.onTouchMove, this), false);
@@ -72,6 +77,7 @@
         /**
          * @private
          * @method calcSelfSize
+         * @desc 监听window.onresize事件
          */
         calcSelfSize: function() {
             if(!this.isResize) {
@@ -83,6 +89,7 @@
         /**
          * @private
          * @method onTouchMove
+         * @desc isScroll为false时 禁掉滚动条的滚动
          */
         onTouchMove: function(e) {
             this.isShow && o.event.stop(e);
@@ -91,6 +98,7 @@
         /**
          * @public
          * @method octopus.Widget.Mask.activate
+         * @desc 复写父类方法 在节点扔进dom流后做的初始化
          */
         activate: function() {
             this.superclass.activate.apply(this, arguments);
@@ -112,8 +120,9 @@
             }
             o.dom.setStyles(this.el, {
                 width: "100%",
-                height: Math.max(this.container.scrollHeight, this.container.clientHeight) - 1 + "px"
-            }, true);
+                left: "0px",
+                height: Math.max(this.container.scrollHeight, this.container.clientHeight) + "px"
+            }, false);
             this.isResize = false;
         },
 
@@ -122,14 +131,11 @@
          * @method octopus.Widget.Mask.show
          */
         show: function() {
-            console.log("show: " + this.isShow);
             if(this.isShow) return;
             this.isShow = true;
             this.el.style.visibility = "visible";
-            if(this.animation) {
-                return new o.Tween(this.el, "opacity", 0, .5, .4, o.util.empty, {
-                    ease: "ease-out"
-                });
+            if(!!this[this.animation]) {
+                this[this.animation](true);
             }
         },
 
@@ -138,19 +144,10 @@
          * @method octopus.Widget.Mask.hidden
          */
         hidden: function() {
-            console.log(this.isShow);
             if(!this.isShow)    return;
             this.isShow = false;
-            console.log("this.animation: " + this.animation);
-            if(this.animation) {
-                try {
-                    new o.Tween(this.el, "opacity", .5, 0, .4, o.util.bind(this._hidden, this), {
-                        ease: "ease-out"
-                    });
-                } catch(e) {
-                    console.log(e.message);
-                }
-
+            if(!!this[this.animation]) {
+                this[this.animation](false);
             } else {
                 this._hidden();
             }
@@ -161,8 +158,114 @@
          * @method _hidden
          */
         _hidden: function() {
-            console.log("sucess");
             this.el.style.visibility = "hidden";
+        },
+
+        /**
+         * @private
+         * @method fade
+         * @param out {Boolean}
+         */
+        fade: function(out) {
+            if(out) {
+                new o.Tween(this.el, "opacity", 0, 1, .4, o.util.empty, {
+                    ease: "ease-out"
+                });
+            } else {
+                new o.Tween(this.el, "opacity", 1, 0, .4, o.util.bind(this._hidden, this), {
+                    ease: "ease-out"
+                });
+            }
+        },
+
+        /**
+         * @private
+         * @method scale
+         * @param out {Boolean}
+         */
+        scale: function(out) {
+            if(out) {
+                new o.Tween(this.el, ["opacity", "-webkit-transform"], [0, "scale(0)"], [1, "scale(1)"], .4);
+            } else {
+                new o.Tween(this.el, ["opacity", "-webkit-transform"], [1, "scale(1)"], [0, "scale(0)"], .4, o.util.bind(this._hidden, this));
+            }
+        },
+
+        /**
+         * @private
+         * @method rotate
+         * @param out {Boolean}
+         */
+        rotate: function(out) {
+            this.el.style.webkitTransformOrigin = "left bottom";
+            var that = this;
+            if(out) {
+                new o.Tween(this.el, ["opacity", "-webkit-transform"], [0, "rotate(-90deg)"], [1, "rotate(0deg)"], .4, function() {
+                    that.el.style.webkitTransformOrigin = "";
+                });
+            } else {
+                new o.Tween(this.el, ["opacity", "-webkit-transform"], [1, "rotate(0deg)"], [0, "rotate(90deg)"], .4, function() {
+                    that._hidden();
+                    that.el.style.webkitTransformOrigin = "";
+                });
+            }
+        },
+
+        /**
+         * @private
+         * @method slideLeft
+         * @param out {Boolean}
+         */
+        slideLeft: function(out) {
+            this.animate("slide", "left", out);
+        },
+
+        /**
+         * @private
+         * @method slideRight
+         * @param out
+         */
+        slideRight: function(out) {
+            this.animate("slide", "right", out);
+        },
+
+        /**
+         * @private
+         * @method slideUp
+         * @param out
+         */
+        slideUp: function(out) {
+            this.animate("slide", "up", out);
+        },
+
+        /**
+         * @private
+         * @method slideDown
+         * @param out
+         */
+        slideDown: function(out) {
+            this.animate("slide", "down", out);
+        },
+
+        /**
+         * @private
+         * @method animate
+         * @param type {String}
+         * @param direction {String}
+         * @param out {Boolean}
+         */
+        animate: function(type, direction, out) {
+            var func = out ? o.util.empty : o.util.bind(this._hidden, this);
+            o.animate({
+                el: this.el,
+                type: type,
+                func: func,
+                config: {
+                    direction: direction,
+                    out: !out,
+                    isFade: true
+                }
+            });
         },
 
         CLASS_NAME: "octopus.Widget.Mask"
