44d822b6dfc0023565234c38dcad67ccbfca391e
yanx <yanx@oupeng.com>
2013-07-26 15:24:29 +0800

    修改scroll的demo 增加resize和横竖屏切换的处理

--- demo/base/scroll/scroll.js
+++ demo/base/scroll/scroll.js
@@ -2,14 +2,25 @@
 
 	"use strict";
 
-	o.event.on(window, "DOMContentLoaded", onLoaded);
+	var limitV;
 
+	o.event.on(window, "DOMContentLoaded", onLoaded);
 	function onLoaded() {
+		calcLimitV();
+		o.dom.scrollLite(document.body)
 		new o.ScrollLite({
 			el: "scroll_container",
 			scrollEl: "scroll_scrollContainer",
 			distance: 400,
-			isTransform: true
+			isTransform: true,
+			maxV: 50,
+			limitV: limitV,
+			swipeVelocity: 0.1
 		});
 	}
+
+	function calcLimitV() {
+		limitV = o.dom.getWidth("scroll_scrollContainer") - o.dom.getWidth("scroll_container");
+	}
+
 })(octopus);
