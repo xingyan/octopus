31c9589ac19987330b52be35ae02a83706b776f5
yanx <yanx@oupeng.com>
2013-09-03 15:11:51 +0800

    加一个原生属性

--- src/widget/slider/slider.js
+++ src/widget/slider/slider.js
@@ -1185,7 +1185,7 @@
             });
             var dom = o.dom.createDom("div", {
                     "class": "octopusui-slider-children",
-                    "style": "-webkit-transform: translate3d(0, 0, 0); position: relative;"
+                    "style": "transform: translate3d(0, 0, 0); -webkit-transform: translate3d(0, 0, 0); position: relative;"
                 }),
                 _item = item.cloneNode(true),
                 _itemcssText = "width: 100%;";
